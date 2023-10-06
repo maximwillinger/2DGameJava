3a040adc3a990866637bc2f545d9a66ba2946b3b
Maxim Willinger <1203@htl.rennweg.at>
2023-10-06 23:21:16 +0200

    Part 5

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -83,10 +83,15 @@ public class TileManager {
             int tileNum = mapTileNum[worldCol][worldRow];
             int worldX = worldCol * gp.tileSize;
             int worldY = worldRow * gp.tileSize;
-            int screenX = worldX - gp.player.worldX + gp.player.worldX;
-            int screenY = worldY - gp.player.worldY + gp.player.worldY;
+            int screenX = worldX - gp.player.worldX + gp.player.screenX;
+            int screenY = worldY - gp.player.worldY + gp.player.screenY;
 
-            g2.drawImage(tile[tileNum].image, screenX, screenY, gp.tileSize, gp.tileSize, null);
+            if (worldX + gp.tileSize > gp.player.worldX - gp.player.screenX &&
+                worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
+                worldY + gp.tileSize > gp.player.worldY - gp.player.screenY &&
+                worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {
+                g2.drawImage(tile[tileNum].image, screenX, screenY, gp.tileSize, gp.tileSize, null);
+            }
             worldCol++;
             if (worldCol == gp.maxWorldCol) {
                 worldCol = 0;
