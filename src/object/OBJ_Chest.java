8cf7534c947964fc918a2d1ea8817007c53bd729
Maxim Willinger <1203@htl.rennweg.at>
2023-10-07 15:24:35 +0200

    Part 7

---
+++ src/object/OBJ_Chest.java
@@ -0,0 +1,16 @@
+package object;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.util.Objects;
+
+public class OBJ_Chest extends SuperObject {
+    public OBJ_Chest() {
+        name = "Chest";
+        try {
+            image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("../res/objects/chest (OLD).png")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/object/OBJ_Door.java
@@ -0,0 +1,16 @@
+package object;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.util.Objects;
+
+public class OBJ_Door extends SuperObject{
+    public OBJ_Door() {
+        name = "Door";
+        try {
+            image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("../res/objects/door.png")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
